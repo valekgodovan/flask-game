149ea6960462cbfdf24791fb0f942483c77ccde6
valek <valekgodovan@gmail.com>
2024-03-04 22:12:29 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from game_of_life import GameOfLife
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    GameOfLife(25, 25)
+    return render_template(
+        'index.html'
+    )
+
+@app.route('/live')
+def live():
+    live = GameOfLife()
+    live.form_new_generation()
+    return render_template(
+        'live.html',
+        live=live,
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
